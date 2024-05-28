ea447d6390a5f41a875b96a578c9a0400b3c829d
Feldon003 <feldonkor04@gmail.com>
2024-05-29 00:26:49 +0100

    first interface done

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style = {styles.logo}>FELVA</Text>
+      <Text style = {styles.logo}>QUADZ™</Text>
       <View style = {styles.mainText}>
-        <Text style = {styles.welcome}>Welcome to Felva</Text>
+        <Text style = {styles.welcome}>Welcome to Quadz</Text>
         <Text style = {styles.introduction}>We are very glad to have you here</Text>
-        <Text style = {styles.option}>Sign-in OR Sign-up to join the felva community</Text>
+        <Text style = {styles.option}>Sign-in OR Sign-up to join the Quadz Family</Text>
       </View>
-      <View style = {styles.buttons}>
+      <View style = {styles.buttonContainer}>
+        <TouchableOpacity style = {styles.button} 
+        onPress={() => console.log("Sign-in button pressed")}>
+          <Text style = {styles.buttonText}>Sign-In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style = {styles.button}
+        onPress={() => console.log("Sign-up button pressed")}>
+          <Text style = {styles.buttonText}>Sign-Up</Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -20,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00ff00',
+    backgroundColor: '#ADD8E6',
   
   },
   logo:{
@@ -51,6 +59,32 @@ const styles = StyleSheet.create({
     textAlign: 'center', 
     fontSize: 25, 
     fontWeight: 'bold',
-    color: '#0000ff'
+    color: '#ffffff',
+    padding: 12,
+  }, 
+  buttonContainer:{
+    flexDirection: 'row', 
+    justifyContent: 'center', 
+    // backgroundColor: '#0000ff',
+    // marginBottom: 150,
+    height: 55,
+  },
+  button: {
+    backgroundColor: '#ffffff',
+    padding: 15, 
+    width: 175, 
+    height: 50, 
+    // justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
+  }, 
+  buttonText: {
+    color: '#000000',
+    fontSize: 24,
+    // backgroundColor: '#ff0000',
+    width: 175,
+    height: 50,
+    textAlign: 'center',
+    justifyContent: 'center', 
   }
 });
